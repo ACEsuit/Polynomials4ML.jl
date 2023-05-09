1eb3c9365858d7f77544a8e0aa3f7e0e9eac543e
cortner <christohortner@gmail.com>
2023-05-09 11:02:55 -0700

    updated RNl

--- src/atomicorbitalsradials/atomicorbitalsradials.jl
+++ src/atomicorbitalsradials/atomicorbitalsradials.jl
@@ -3,50 +3,58 @@ export AtomicOrbitalsRadials, GaussianBasis, SlaterBasis, STO_NG
 const NLM{T} = NamedTuple{(:n1, :n2, :l, :m), Tuple{T, T, T, T}}
 const NL{T} = NamedTuple{(:n1, :n2, :l), Tuple{T, T, T}}
 
-struct AtomicOrbitalsRadials{TP, TD, TI, TZ} 
+struct AtomicOrbitalsRadials{TP, TD, TI, TZ}  <: AbstractPoly4MLBasis
    Pn::TP
    Dn::TD
    spec::Vector{NL{TI}}
-   ζ::Vector{TZ}
+   ζ::Vector{TZ}   # later : this does into a parameters named-tuple 
    # ----------------- metadata 
-   meta::Dict{String, Any}
+   @reqfields
 end
 
-AtomicOrbitalsRadials(Pn, Dn, spec, ζ; meta = Dict{String, Any}()) = 
-        AtomicOrbitalsRadials(Pn, Dn, spec, ζ, meta)
+AtomicOrbitalsRadials(Pn, Dn, spec, ζ) = 
+        AtomicOrbitalsRadials(Pn, Dn, spec, ζ, _make_reqfields()...)
 
 Base.length(basis::AtomicOrbitalsRadials) = length(basis.spec)
 
+_valtype(basis::AtomicOrbitalsRadials, T::Type{<: Real}) = T
+
 # -------- Evaluation Code 
-_alloc(basis::AtomicOrbitalsRadials, r::T) where {T <: Real} = 
-      zeros(T, length(basis))
 
-_alloc(basis::AtomicOrbitalsRadials, rr::Vector{T}) where {T <: Real} = 
-      zeros(T, length(rr), length(basis))
+# TODO: (Jerry?) this kind of construction could be used for all  bases? 
+#       file an issue on this.
 
-evaluate(basis::AtomicOrbitalsRadials, r::Number) = evaluate(basis, [r,])[:]
+function evaluate!(Rnl, basis::AtomicOrbitalsRadials, r::Number)
+    Rnl_ = reshape(Rnl, (1, length(basis)))
+    evaluate!(Rnl_, basis, [r,])
+    return Rnl 
+end
 
-function evaluate(basis::AtomicOrbitalsRadials, R::AbstractVector{<: Real})
+
+function evaluate!(Rnl, basis::AtomicOrbitalsRadials, R::AbstractVector{<: Real})
     nR = length(R)
-    Pn = Polynomials4ML.evaluate(basis.Pn, R) # Pn(r)
-    Dn = evaluate(basis.Dn, basis.ζ, R) # Dn(r)
-    Rnl = _alloc(basis, R) 
+    Pn = evaluate(basis.Pn, R)           # Pn(r)
+    Dn = evaluate(basis.Dn, basis.ζ, R)  # Dn(r)  (ζ are the parameters -> reorganize the Lux way)
 
+    fill!(Rnl, 0)
+    
     for (i, b) in enumerate(basis.spec)
         for j = 1:nR
             Rnl[j, i] = Pn[j, b.n1] * Dn[j, i]
         end
     end
 
+    release!(Pn); release!(Dn)
+
     return Rnl 
 end
 
-function evaluate_ed(basis::AtomicOrbitalsRadials, R)
-    nR = length(R) 
-    Pn, dPn = Polynomials4ML.evaluate_ed(basis.Pn, R)
+function evaluate_ed!(Rnl, dRnl, basis::AtomicOrbitalsRadials, R)
+    nR = length(R)
+    Pn, dPn = evaluate_ed(basis.Pn, R)
     Dn, dDn = evaluate_ed(basis.Dn, basis.ζ, R)
-    Rnl = _alloc(basis, R)
-    dRnl = _alloc(basis, R)
+
+    fill!(Rnl, 0); fill!(dRnl, 0); 
 
     for (i, b) in enumerate(basis.spec)
         for j = 1:nR
@@ -55,17 +63,19 @@ function evaluate_ed(basis::AtomicOrbitalsRadials, R)
             dRnl[j, i] += Pn[j, b.n1] * dDn[j, i]
         end
     end
+
+    release!(Pn); release!(dPn); release!(Dn); release!(dDn)
+
     return Rnl, dRnl 
 end
 
 
-function evaluate_ed2(basis::AtomicOrbitalsRadials, R)
-    nR = length(R) 
-    Pn, dPn, ddPn = Polynomials4ML.evaluate_ed2(basis.Pn, R)
+function evaluate_ed2!(Rnl, dRnl, ddRnl, basis::AtomicOrbitalsRadials, R)
+    nR = length(R)
+    Pn, dPn, ddPn = evaluate_ed2(basis.Pn, R)
     Dn, dDn, ddDn = evaluate_ed2(basis.Dn, basis.ζ, R)
-    Rnl = _alloc(basis, R)
-    dRnl = _alloc(basis, R)
-    ddRnl = _alloc(basis, R)
+
+    fill!(Rnl, 0); fill!(dRnl, 0); fill!(ddRnl, 0)
 
     for (i, b) in enumerate(basis.spec)
         for j = 1:nR
@@ -74,6 +84,10 @@ function evaluate_ed2(basis::AtomicOrbitalsRadials, R)
             ddRnl[j, i] += ddPn[j, b.n1] * Dn[j, i] + 2 * dPn[j, b.n1] * dDn[j, i] + Pn[j, b.n1] * ddDn[j, i]
         end
     end
+
+    release!(Pn); release!(dPn); release!(ddPn); 
+    release!(Dn); release!(dDn); release!(ddDn)
+
     return Rnl, dRnl, ddRnl
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,25 @@ using Polynomials4ML
 using Test
 
 @testset "Polynomials4ML.jl" begin 
+
+    # 1D Polynomials 
     @testset "OrthPolyBasis1D3T" begin include("test_op1d3t.jl"); end
     @testset "DiscreteWeights" begin include("test_discreteweights.jl"); end
+
+    # 2D Harmonics 
     @testset "TrigonometricPolynomials" begin include("test_trig.jl"); end
     @testset "Real Trig Polys" begin include("test_rtrig.jl"); end
 
+    # 3D Harmonics 
     @testset "Complex SphericalHarmonics" begin include("test_cylm.jl"); end
     @testset "Real Spherical Harmonics" begin include("test_rylm.jl"); end
     @testset "Complex Solid Harmonics" begin include("test_crlm.jl"); end
     @testset "Real Solid Harmonics" begin include("test_rrlm.jl"); end
 
-    # @testset "Rnl" begin include("test_rnl.jl"); end
-    
+    # Quantum Chemistry 
+    @testset "Rnl" begin include("test_rnl.jl"); end
+
+    # Misc
     @testset "Flex Array Interface" begin include("test_flex.jl"); end 
     @testset "Sparse Product" begin include("test_sparseproduct.jl"); end 
     @testset "Lux" begin include("test_lux.jl"); end 
