7c0b6cac16e12255a467b0e5c588e8419bb5dd87
cortner <christohortner@gmail.com>
2023-06-14 18:04:16 -0700

    Polyester experiment

--- profile/test_mt_A.jl
+++ profile/test_mt_A.jl
@@ -1,10 +1,12 @@
 
 using BenchmarkTools, Test, Polynomials4ML
-using Polynomials4ML: PooledSparseProduct, evaluate, evaluate!
-using StrideArrays, LoopVectorization, Tullio 
+using Polynomials4ML: PooledSparseProduct, evaluate, evaluate!, BB_prod
+using StrideArrays, LoopVectorization, Tullio, Polyester
 using Main.Threads
 
 P4ML = Polynomials4ML
+
+##
 
 function eval_mt1!(A, basis::PooledSparseProduct, BB)
    nBB = length(BB)
@@ -43,6 +45,30 @@ function eval_mt2!(A, basis::PooledSparseProduct, BB)
    return nothing 
 end 
 
+
+function eval_mt3!(A, basis::PooledSparseProduct, BB)
+   nBB = length(BB)
+   nA = length(basis) 
+   nX = size(BB[1], 1) 
+   @assert length(A) >= nA 
+   @assert all(size(BB[i], 1) == nX for i = 2:nBB)
+
+   B1, B2, B3 = BB      
+
+   @batch for iA = 1:nA
+      a = 0.0 
+      b = basis.spec[iA]
+      @avx for iX = 1:nX 
+         a += BB_prod(b, BB)
+      end
+      @inbounds A[iA] = a 
+   end
+
+   return nothing 
+end 
+
+
+
 ##
 
 N1 = 10
@@ -60,21 +86,22 @@ basis = PooledSparseProduct(spec)
 ## 
 
 nX = 64
-B1 = randn(nX, N1)
-B2 = randn(nX, N2)
-B3 = randn(nX, N3)
+_B1 = randn(nX, N1); _B2 = randn(nX, N2); _B3 = randn(nX, N3)
+B1 = PtrArray(_B1); B2 = PtrArray(_B2); B3 = PtrArray(_B3)
+_A0 = zeros(length(basis)); _A1 = zeros(length(basis)); _A2 = zeros(length(basis))
+A0 = PtrArray(_A0); A1 = PtrArray(_A1); A2 = PtrArray(_A2)
+
 BB = (B1, B2, B3)
-A0 = zeros(length(basis))
-A1 = zeros(length(basis))
-A2 = zeros(length(basis))
 
 evaluate!(A0, basis, BB)
 eval_mt1!(A1, basis, BB)
-eval_mt2!(A2, basis, BB)
-A0 ≈ A1 
+eval_mt3!(A2, basis, BB)
+A0 ≈ A1
 A0 ≈ A2
+
 
 ##
 
-@btime evaluate!($A1, $basis, $BB)
-@btime eval_mt1!($A2, $basis, $BB)
+@btime evaluate!($A0, $basis, $BB)
+@btime eval_mt1!($A1, $basis, $BB)
+@btime eval_mt3!($A2, $basis, $BB)
