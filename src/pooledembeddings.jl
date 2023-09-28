08227f4c6b5c13ab40e5318e5b6fe9ded4f3c57e
cheukhinhojerry <cheukhinhojerry@gmail.com>
2023-09-28 01:08:06 -0700

    pb working, tests later

--- src/pooledembeddings.jl
+++ src/pooledembeddings.jl
@@ -1,6 +1,10 @@
-using Polynomials4ML: AbstractPoly4MLBasis, PooledSparseProduct
-
 export PooledEmebddings
+
+using ChainRulesCore: NoTangent
+using Polynomials4ML: PooledSparseProduct, AbstractPoly4MLBasis
+
+import ChainRulesCore: rrule
+import Base.Cartesian: @nexprs
 
 struct PooledEmebddings{NB, TB <: Tuple} <: AbstractPoly4MLBasis
    embeddings::TB
@@ -12,7 +16,7 @@ function PooledEmebddings(embeddings, pooling)
    return PooledEmebddings(embeddings, pooling, _make_reqfields()...)
 end
 
-import Base.Cartesian: @nexprs
+Base.length(basis::PooledEmebddings) = length(basis.pooling)
 
 function _write_code_Bi_tup(NB)
    Bi_tup_str = "(B_1, "
@@ -20,19 +24,59 @@ function _write_code_Bi_tup(NB)
       Bi_tup_str *= "B_$i, "
    end
    Bi_tup_str *= "B_$NB)"
-   return Bi_tup_str
+   return Meta.parse(Bi_tup_str)
 end
 
-@generated function evaluate(basis::PooledEmebddings{NB, TB}, X)
+# TODO: generalize to any X
+@generated function evaluate(basis::PooledEmebddings{NB, TB}, X::Vector{SVector{3, T}}) where {NB, TB, T}
    @assert NB > 0
+   B_tup = _write_code_Bi_tup(NB)
    quote
       @nexprs $NB i -> begin
          embed_i = basis.embeddings[i]
          B_i = evaluate(embed_i, X)
       end
-      B_tup = _write_code_Bi_tup(NB)
-      A = basis.pooling($B_tup)
+      A = evaluate(basis.pooling, $B_tup)
       @nexprs $NB i -> release!(B_i)
       return A
+   end
+end
+
+function ChainRulesCore.rrule(::typeof(evaluate), basis::PooledEmebddings{NB, TB}, X::Vector{SVector{3, T}}) where {NB, TB, T}
+   @show "Calling correct rrule"
+   A = evaluate(basis, X)
+   return A, Δ -> _evaluate_pb(basis, Δ, X)
+end
+
+@generated function _evaluate_pb(basis::PooledEmebddings{NB, TB}, Δ, X) where {NB, TB}
+   B_tup = _write_code_Bi_tup(NB)
+   quote
+      # evaluate
+      @nexprs $NB i -> begin
+         embed_i = basis.embeddings[i]
+         B_i = evaluate(embed_i, X)
+      end
+      A = evaluate(basis.pooling, $B_tup)
+
+      # pooling
+      pooling_out, pooling_pb = ChainRulesCore.rrule(evaluate, basis.pooling, $B_tup)
+
+      # get the embedding pullbacks
+      @nexprs $NB i -> begin
+         _, embed_pb_i = ChainRulesCore.rrule(evaluate, basis.embeddings[i], X)    
+      end
+      
+      # pooling_pb : Vec -> (B_1, B_2, ..., B_NB)
+      # embed_pb_i : B_i -> ∂X
+      # use 3 since the interface must return (NoTangent(), NoTangent(), ∂)
+      ∂X = zero(X)
+      ∂BBs = pooling_pb(Δ)[3]
+      # writes and accumulate to ∂X
+      @nexprs $NB i -> begin
+         ∂X_i = embed_pb_i(∂BBs[i])
+         ∂X .+= ∂X_i[3]
+      end
+      @show ∂X
+      return (NoTangent(), NoTangent(), ∂X)
    end
 end
--- test/test_pooledembeddings.jl
+++ test/test_pooledembeddings.jl
@@ -1,84 +1,4 @@
-
-
-module PooledEmbed
-using Polynomials4ML: AbstractPoly4MLBasis, PooledSparseProduct
-using Polynomials4ML: @reqfields, POOL, TMP, _make_reqfields, META, evaluate
-using Polynomials4ML
-using StaticArrays
-using ObjectPools: release!, acquire!
-using ChainRulesCore
-struct PooledEmebddings{NB, TB <: Tuple} <: AbstractPoly4MLBasis
-   embeddings::TB
-   pooling::PooledSparseProduct{NB}
-   @reqfields()
-end
-
-function PooledEmebddings(embeddings, pooling)
-   return PooledEmebddings(embeddings, pooling, _make_reqfields()...)
-end
-
-Base.length(basis::PooledEmebddings) = length(basis.pooling)
-
-import Base.Cartesian: @nexprs
-
-function _write_code_Bi_tup(NB)
-   Bi_tup_str = "(B_1, "
-   for i = 2:NB-1
-      Bi_tup_str *= "B_$i, "
-   end
-   Bi_tup_str *= "B_$NB)"
-   return Meta.parse(Bi_tup_str)
-end
-
-@generated function myevaluate(basis::PooledEmebddings{NB, TB}, X::Vector{SVector{3, T}}) where {NB, TB, T}
-   @assert NB > 0
-   B_tup = _write_code_Bi_tup(NB)
-   quote
-      @nexprs $NB i -> begin
-         embed_i = basis.embeddings[i]
-         B_i = evaluate(embed_i, X)
-      end
-      A = evaluate(basis.pooling, $B_tup)
-      @nexprs $NB i -> release!(B_i)
-      return A
-   end
-end
-
-function ChainRulesCore.rrule(::typeof(myevaluate), basis::PooledEmebddings{NB, TB}, X::Vector{SVector{3, T}}) where {NB, TB, T}
-   A = myevaluate(basis, X)
-   return A, Δ -> _evaluate_pb(basis, Δ, X)
-end
-
-@generated function _evaluate_pb(basis::PooledEmebddings{NB, TB}, Δ, X) where {NB, TB}
-   B_tup = _write_code_Bi_tup(NB)
-   quote
-      @nexprs $NB i -> begin
-         embed_i = basis.embeddings[i]
-         B_i = evaluate(embed_i, X)
-      end
-      A = evaluate(basis.pooling, $B_tup)
-      pooling_out, pooling_pb = ChainRulesCore.rrule(evaluate, basis.pooling, $B_tup)
-      @nexprs $NB i -> begin
-         _, embed_pb_i = ChainRulesCore.rrule(evaluate, basis.embeddings[i], X)    
-      end
-      # pooling_pb : Vec -> (B_1, B_2, ..., B_NB)
-      # embed_pb_i : B_i -> ∂X
-      ∂X = similar(X)
-      BBs = pooling_pb(Δ)[3]
-      @show typeof(BBs)
-      @nexprs $NB i -> begin
-         ∂X_i = embed_pb_i(BBs[i])
-         @show typeof(∂X_i)
-         ∂X .+= ∂X_i[3]
-      end
-      @show ∂X
-      return ∂X
-   end
-end
-
-end # module PooledEmbed
-
-using Polynomials4ML: legendre_basis, RYlmBasis, ScalarPoly4MLBasis, PooledSparseProduct, OrthPolyBasis1D3T
+using Polynomials4ML: legendre_basis, RYlmBasis, ScalarPoly4MLBasis, PooledSparseProduct, OrthPolyBasis1D3T, PooledEmebddings
 using StaticArrays, LinearAlgebra
 using ObjectPools: acquire!, release!
 using Polynomials4ML
@@ -99,13 +19,15 @@ function Polynomials4ML.evaluate!(Y::AbstractArray, basis::OrthPolyBasis1D3T, X:
    release!(R)
 	return Y
 end
+
 using ChainRulesCore
+
 function ChainRulesCore.rrule(::typeof(Polynomials4ML.evaluate), basis::OrthPolyBasis1D3T, X::Vector{SVector{3, T}}) where T
    R = norm.(X)
    P, dP = evaluate_ed(basis, R)
 
    function pb(∂)
-      ∂X = similar(X)
+      ∂X = zero(X)
       for n = 1:size(dP, 2)
          @simd ivdep for a = 1:length(X)
             ∂X[a] += ∂[a, n] * dP[a, n] * X[a] / R[a]
@@ -114,7 +36,7 @@ function ChainRulesCore.rrule(::typeof(Polynomials4ML.evaluate), basis::OrthPoly
       return (NoTangent(), NoTangent(), ∂X)
    end
 
-  return Y, pb
+  return P, pb
 end
 
 Polynomials4ML._valtype(basis::OrthPolyBasis1D3T{T}, ::Type{<: StaticVector{3, S}}) where {T <: Real, S <: Real} = promote_type(T, S)
@@ -127,9 +49,9 @@ pooling = _generate_basis()
 X = [ @SVector(randn(3)) for i in 1:3 ]
 
 embeddings = (Rnl, Ylm)
-embed_and_pool = PooledEmbed.PooledEmebddings(embeddings, pooling)
-Main.PooledEmbed.myevaluate(embed_and_pool, X)
+embed_and_pool = Polynomials4ML.PooledEmebddings(embeddings, pooling)
+# Polynomials4ML.PooledEmbed.myevaluate(embed_and_pool, X)
 using Zygote
-out, pb = Zygote.pullback(X -> Main.PooledEmbed.myevaluate(embed_and_pool, X), X)
+out, pb = Zygote.pullback(evaluate, embed_and_pool, X)
 
 pb(out)
