cdbe1121ae6ec0fc2e84c5182dd261cd21882668
cheukhinhojerry <cheukhinhojerry@gmail.com>
2024-03-06 15:26:28 -0800

    add testing script

--- test/sphericalharmonics/test_scrrlm.jl
+++ test/sphericalharmonics/test_scrrlm.jl
@@ -5,8 +5,22 @@ using Polynomials4ML: evaluate, evaluate_d, evaluate_ed
 using Polynomials4ML.Testing: print_tf, println_slim 
 using ACEbase.Testing: fdtest
 using HyperDualNumbers: Hyper
+using OffsetArrays
 
 
+import SpheriCart: generate_Flms
+
+function generate_Flms(L::Integer; normalisation = :p4ml, T = Float64)
+   Flm = OffsetMatrix(zeros(L+1, L+1), (-1, -1))
+   @show "new2"
+   for l = 0:L
+      Flm[l, 0] = sqrt(2)
+      for m = 1:l 
+         Flm[l, m] = Flm[l, m-1] / sqrt((l+m) * (l+1-m))
+      end
+   end
+   return Flm
+end
 
 verbose = false
 
@@ -14,17 +28,20 @@ verbose = false
 
 ##
 
-_maxL = 20
-cSH = RRlmBasis(_maxL)
-rSH = SCRRlmBasis(_maxL)
+_maxL = 3
+p4ml_rrlm = RRlmBasis(_maxL)
+sc_rrlm = SCRRlmBasis(_maxL)
 
 _R = rand_sphere()
-_scale = evaluate(cSH, _R) ./ evaluate(rSH, _R)
+_scale = evaluate(p4ml_rrlm, _R) ./ evaluate(sc_rrlm, _R)
+
+
+
 for nsamples = 1:30
    local R 
    R = rand_sphere()
-   cY = evaluate(cSH, R)
-   rY = evaluate(rSH, R)
+   cY = evaluate(p4ml_rrlm, R)
+   rY = evaluate(sc_rrlm, R)
    print_tf(@test cY ./ rY ≈ _scale)
 end
 println()
@@ -35,10 +52,10 @@ println()
 @info("Check consistency of serial and batched evaluation")
 
 X = [ rand_sphere() for i = 1:23 ]
-Y1 = evaluate(rSH, X)
+Y1 = evaluate(sc_rrlm, X)
 Y2 = similar(Y1) 
 for i = 1:length(X)
-   Y2[i, :] = evaluate(rSH, X[i])
+   Y2[i, :] = evaluate(sc_rrlm, X[i])
 end
 println_slim(@test Y1 ≈ Y2)
 
@@ -47,10 +64,10 @@ println_slim(@test Y1 ≈ Y2)
 
 @info("Test: check derivatives of real spherical harmonics")
 for nsamples = 1:30
-   local R, rSH, h 
+   local R, sc_rrlm, h 
    R = @SVector rand(3)
-   rSH = SCRRlmBasis(5)
-   Y, dY = evaluate_ed(rSH, R)
+   sc_rrlm = SCRRlmBasis(5)
+   Y, dY = evaluate_ed(sc_rrlm, R)
    DY = Matrix(transpose(hcat(dY...)))
    errs = []
    verbose && @printf("     h    | error \n")
@@ -60,7 +77,7 @@ for nsamples = 1:30
       Rh = Vector(R)
       for i = 1:3
          Rh[i] += h
-         DYh[:, i] = (evaluate(rSH, SVector(Rh...)) - Y) / h
+         DYh[:, i] = (evaluate(sc_rrlm, SVector(Rh...)) - Y) / h
          Rh[i] -= h
       end
       push!(errs, norm(DY - DYh, Inf))
@@ -76,7 +93,7 @@ println()
 
 @info("Check consistency of serial and batched gradients")
 
-rSH = SCRRlmBasis(10)
+sc_rrlm = SCRRlmBasis(10)
 X = [ rand_sphere() for i = 1:21 ]
 
 x2dualwrtj(x, j) = SVector{3}([Hyper(x[i], i == j, i == j, 0) for i = 1:3])
@@ -84,12 +101,12 @@ x2dualwrtj(x, j) = SVector{3}([Hyper(x[i], i == j, i == j, 0) for i = 1:3])
 hX = [x2dualwrtj(x, 1) for x in X]
 
 
-Y0 = evaluate(rSH, X)
-Y1, dY1 = evaluate_ed(rSH, X)
+Y0 = evaluate(sc_rrlm, X)
+Y1, dY1 = evaluate_ed(sc_rrlm, X)
 Y2 = similar(Y1); dY2 = similar(dY1)
 for i = 1:length(X)
-   Y2[i, :] = evaluate(rSH, X[i])
-   dY2[i, :] = evaluate_ed(rSH, X[i])[2]
+   Y2[i, :] = evaluate(sc_rrlm, X[i])
+   dY2[i, :] = evaluate_ed(sc_rrlm, X[i])[2]
 end
 println_slim(@test Y0 ≈ Y1 ≈ Y2)
 println_slim(@test dY1 ≈ dY2)
